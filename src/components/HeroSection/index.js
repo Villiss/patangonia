90d28eda4ddc0e3e5e70706560e9596ae388e281
Villiss <lukas.vilim@outlook.com>
2021-07-22 16:24:20 +0200

    vela novych veci

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,37 @@
+import React, {useState} from 'react'
+import { HeroBg, HeroContainer, VideoBg, ArrowForward, ArrowRight, HeroBtnWrapper, HeroContent, HeroH1, HeroP } from './HeroElements'
+import Video from '../../video/video.mp4'
+import { Button } from '../ButtonElements'
+
+const HeroSection = () => {
+
+    const [hover, setHover] = useState(false)
+
+    const onHover = () => {
+        setHover(!hover)
+    }
+
+    return (
+        <div>
+            <HeroContainer id='home'>
+                <HeroBg>
+                    <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+                </HeroBg>
+                <HeroContent>
+                    <HeroH1>Yerba Maté</HeroH1>
+                    <HeroP>
+                        Sign Up and learn everything.
+                    </HeroP>
+                    <HeroBtnWrapper>
+                        <Button to='signup' onMouseEnter={onHover} onMouseLeave={onHover}
+                        primary='true' dark='true'>
+                            Get Started {hover ? <ArrowForward /> : <ArrowRight/>}
+                        </Button>
+                    </HeroBtnWrapper>
+                </HeroContent>
+            </HeroContainer>
+        </div>
+    )
+}
+
+export default HeroSection
