9b2b2d7caf2697edd2209f6e27bd07bf20ca7e06
Villiss <lukas.vilim@outlook.com>
2021-07-23 09:59:26 +0200

    info section done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,12 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom'
 import Home from './pages';
-import HeroSection from './components/HeroSection';
 
 
 function App() {
   return (
     <Router>
       <Home />
-      <HeroSection/>
     </Router>
   );
 }
