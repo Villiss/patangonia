e3d681d59b72fa3ddc38ae5d1fbab21ef079e34c
Lukáš Vilim <l.vilim18@gmail.com>
2021-12-07 16:56:16 +0100

    text na stred pri mobile

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -30,6 +30,7 @@ export const InfoRow = styled.div`
 
     @media screen and (max-width: 810px){
         grid-template-areas: ${({imgStart})=> imgStart ? `'col1' 'col2'` : `'col1 col1' 'col2 col2'`};
+        text-align: center;
     }
 `;
 
